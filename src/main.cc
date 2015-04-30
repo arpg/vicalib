f715188d048f7ce0248c145b4a7a83b97b587497
Gabe Sibley <gsibley@gwu.edu>
2015-04-30 13:34:06 -0600

    fix usage

--- src/main.cc
+++ src/main.cc
@@ -5,14 +5,24 @@
 #include <glog/logging.h>
 #include <vicalib/vicalib-engine.h>
 
-int main(int argc, char *argv[]) {
-  google::ParseCommandLineFlags(&argc, &argv, true);
+#define USAGE \
+
+int main(int argc, char *argv[]) 
+{
+  if( argc == 1 ){
+    google::SetUsageMessage("Vicalib options:");
+    google::ShowUsageWithFlags(argv[0]);
+    return -1;
+  }
+  
   google::InitGoogleLogging(argv[0]);
+  google::ParseCommandLineFlags(&argc, &argv, true);
 
   LOG(INFO) << "Starting vicalib engine.";
   visual_inertial_calibration::VicalibEngine engine(
       [](){}, [](const std::shared_ptr<
                  visual_inertial_calibration::CalibrationStats>&){});
   engine.Run();
+ 
   return 0;
 }
